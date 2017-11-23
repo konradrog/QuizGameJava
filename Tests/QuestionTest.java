5714068a1ec9333840a70a39c03fadf54c9726c5
konradrog <konrad1210@gmail.com>
2017-11-23 14:02:36 +0100

    Refactoring test for Question class

--- Tests/QuestionTest.java
+++ Tests/QuestionTest.java
@@ -1,59 +1,62 @@
-import org.junit.Test;
-
+import org.junit.After;
+import org.junit.Before;
 import java.util.ArrayList;
-
 import static org.junit.Assert.*;
-/* wczytywanie plik, i sprawdzanie pliku. zapis do .jar*, drugie konto do repo*/
-public class QuestionTest {
-    private Answer answer1;
-    {
-        answer1 = new Answer();
+
+public class QuestionTest extends junit.framework.TestCase {
+    int corrAns = 0;
+    ArrayList<Answer> answers;
+    Question question;
+
+    public QuestionTest() {}
+
+    protected void setUp() {
+        Answer answer1 = new Answer();
+        Answer answer2 = new Answer();
+        Answer answer3 = new Answer();
+        Answer answer4 = new Answer();
+
         answer1.setText("Pytanie");
         answer1.setValue("0");
-    }
-    private Answer answer2;
-    {
-        answer2 = new Answer();
+
         answer2.setText("Pytanie");
         answer2.setValue("0");
-    }
-    private Answer answer3;
-    {
-        answer3 = new Answer();
 
         answer3.setText("Pytanie");
         answer3.setValue("0");
-    }
-    private Answer answer4;
-    {
-        answer4 = new Answer();
+
         answer4.setText("Pytanie");
         answer4.setValue("1");
-    }
 
-    ArrayList<Answer> answers = new ArrayList<Answer>();
-    {
+        answers = new ArrayList<Answer>();
+
         answers.add(answer1);
         answers.add(answer2);
         answers.add(answer3);
         answers.add(answer4);
-    };
 
-    int corrAns = 0;
+        question = new Question("Pytanie", answers);
+    }
 
-    private Question question = new Question("Pytanie", answers);
+    protected void tearDown() {
+        answers.clear();
+    }
 
-    @Test
-    public void getText() throws Exception {
+
+    public void testGetText() throws Exception {
         assertEquals("Pytanie", question.getText());
     }
-    @Test
-    public void setCorrectAnswer() throws Exception {
+
+
+    public void testSetCorrectAnswer() throws Exception {
         assertEquals(3, question.setCorrAns(answers));
     }
 
-    @Test
-    public void getCorrectAnswer() throws Exception {
+
+    public void testGetCorrectAnswer() throws Exception {
         assertEquals(3, question.getCorrAns());
     }
+
+
+
 }
